6b9f7d7194da9039b0a3066b7a91a9cad89f0d10
Cristi <crissti.cta@gmail.com>
2021-11-04 22:19:36 +0200

    .

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
+
 const express = require('express')
+const path = require('path/posix')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+
+
+
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
