e037de4373f1a40fba034bf382acc7645e75bd36
Cristi <crissti.cta@gmail.com>
2021-11-05 10:03:03 +0200

    .

--- server.js
+++ server.js
@@ -29,11 +29,16 @@ const port = 3000
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
 });
+
+app.get('/user', (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+})
 
 
 
